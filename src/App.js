a2e8841d9a0cf6a7e9960dd31bbd2eb6b6c2f835
Zeyneptirak <zeyneptirak33@gmail.com>
2023-01-30 20:50:20 +0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./screen/HomePage";
+import Design from './servicesPages/Design';
+import Website from './servicesPages/Website'
+import YazilimDestegi from './servicesPages/YazilimDestegi'
+import Eticaret from './servicesPages/Eticaret'
+import Mobil from './servicesPages/Mobil'
+import Egitim from './servicesPages/Egitim'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-card-bg to-right-bg px-[16px] bg-cover">
+      
+    
+        <Router>
+          <div className=''>
+          
+            <Routes>
+              <Route path='/' element={<HomePage/>}></Route>
+              <Route path='/design' element={<Design/>}></Route>
+              <Route path='/website' element={<Website/>}></Route>
+              <Route path='/yazilim' element={<YazilimDestegi/>}></Route>
+              <Route path='/eticaret' element={<Eticaret/>}></Route>
+              <Route path='/mobil' element={<Mobil/>}></Route>
+              <Route path='/egitim' element={<Egitim/>}></Route>
+
+            </Routes>
+            
+          </div>
+        </Router>
+
     </div>
   );
 }
