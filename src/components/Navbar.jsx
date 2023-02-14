1fd42b2c2af2f35a7a0ada5e831aa9489e05c2bc
Zeyneptirak <zeyneptirak33@gmail.com>
2023-02-14 17:20:01 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,17 +12,17 @@ const handleClose=()=> setNav(!nav)
 
   return (
     <div name="navbar" className='w-screen h-[80px] z-10 font-sora absolute top-0'> 
-        <div className='px-2 flex items-center w-full h-full justify-between'>
+        <div className='px-4 md:px-3 flex items-center w-full h-full justify-between'>
             <div className='z-10'>
                 <img className='h-[32px] md:h-[48px] md:ml-16' src={logo} alt="" />
             </div>
             <div className='items-center hidden md:flex mr-16'>
                 <ul className='flex text-[16px] font-[400] z-10'>
-                    <li className='cursor-pointer text-white font-[600]'><Link to="home" smooth={true} duration={500}>Ana Sayfa</Link></li>
-                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey'><Link to="services" smooth={true} offset={-50} duration={500}>Hizmetlerimiz</Link></li>
-                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey'><Link to="about" smooth={true} offset={-200} duration={500}>Hakkımızda</Link></li>
-                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey'><Link to="works" smooth={true} offset={-50} duration={500}>Çalışmalar</Link></li>
-                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey'><Link to="contact" smooth={true} offset={-100} duration={500}>İletişim</Link></li>
+                    <li className='cursor-pointer text-white font-[600] tracking-[2px]'><Link to="home" smooth={true} duration={500}>Ana Sayfa</Link></li>
+                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey tracking-[1px]'><Link to="services" smooth={true} offset={-50} duration={500}>Hizmetlerimiz</Link></li>
+                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey tracking-[1px]'><Link to="about" smooth={true} offset={-200} duration={500}>Hakkımızda</Link></li>
+                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey tracking-[1px]'><Link to="works" smooth={true} offset={-50} duration={500}>Çalışmalar</Link></li>
+                    <li className='cursor-pointer hover:text-white hover:font-[600] text-content-grey tracking-[1px]'><Link to="contact" smooth={true} offset={-100} duration={500}>İletişim</Link></li>
                 </ul>
                 <div className='border-l-2 pl-4 border-content-grey flex items-center'>
                 <a href="https://www.instagram.com/incir.dev/" className="text-content-yellow mr-3 flex items-center" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
